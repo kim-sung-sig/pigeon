18a14f16514c118d397917f74ce87ff815bf3660
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2023-09-09 19:26:07 +0900

    Add files via upload

--- js/stript.js
+++ js/stript.js
@@ -1,6 +1,4 @@
 $(function(){
-
-
   //nav
   $("header").mouseover(function(){
     $(".headerhelper , nav>ul>li>ul").stop().slideDown()
@@ -8,4 +6,20 @@ $(function(){
   $("header").mouseleave(function(){
     $(".headerhelper , nav>ul>li>ul").stop().slideUp()
   })
+})
+
+$(function(){
+  //slide
+  let i =0;
+  $(".curnum").text("0"+1)
+  $(".fullnum").text("/"+"0"+$(".slider>ul>li").length)
+  function slide (){
+    i++;
+    console.log( $(".slider>ul>li").length )
+    $(".slider>ul>li").fadeOut("fast",'linear')
+    $(".slider>ul>li").eq(i%($(".slider>ul>li").length)).fadeIn("fast",'linear')
+    $(".curnum").text("0"+((i)%($(".slider>ul>li").length) +1));
+  }
+  setInterval(slide,4000)
+
 })
